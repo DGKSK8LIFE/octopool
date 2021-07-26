ca746812f49380869a70a5eea5abe7cb3409e547
burntcarrot <aadhav.n1@gmail.com>
2021-07-26 10:02:31 +0530

    tests(octopus): added tests for handling jobs

--- octopus_test.go
+++ octopus_test.go
@@ -16,6 +16,7 @@ package octopool
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -34,4 +35,66 @@ func TestInvalidOctopus(t *testing.T) {
 
 	assert.NotNil(t, testOctopus, "octopus should not be nil")
 	assert.Equal(t, 1000, testOctopus.workerPool.capacity, "octopus should have the default capacity (1000).")
+}
+
+// Test for checking the behavior when an octopus with an negative capacity is created.
+func TestNegativeOctopus(t *testing.T) {
+	testOctopus := NewOctopus(-1)
+
+	assert.NotNil(t, testOctopus, "octopus should not be nil")
+	assert.Equal(t, 1000, testOctopus.workerPool.capacity, "octopus should have the default capacity (1000).")
+}
+
+// Test for checking the behavior when an octopus is given a job.
+func TestOctopusHandleJob(t *testing.T) {
+	testOctopus := NewOctopus(10)
+
+	job1 := func() {
+		time.Sleep(1 * time.Microsecond)
+	}
+
+	testOctopus.HandleJob(job1, "job 1")
+
+	assert.Equal(t, 1, testOctopus.ActiveWorkers())
+}
+
+// Test for checking the behavior when an octopus is given a job when the pool is closed.
+func TestOctopusHandleJobClosedPool(t *testing.T) {
+	testOctopus := NewOctopus(10)
+	testOctopus.Close()
+
+	job1 := func() {
+		time.Sleep(1 * time.Microsecond)
+	}
+
+	err := testOctopus.HandleJob(job1, "job 1")
+
+	assert.Equal(t, "cannot assign job to closed pool", err.Error())
+}
+
+// Test for checking the behavior when an octopus is given a job when the pool is full.
+func TestOctopusHandleJobFullPool(t *testing.T) {
+	testOctopus := NewOctopus(1)
+
+	job1 := func() {
+		time.Sleep(1 * time.Microsecond)
+	}
+
+	job2 := func() {
+		time.Sleep(2 * time.Microsecond)
+	}
+
+	testOctopus.HandleJob(job1, "job 1")
+	testOctopus.HandleJob(job2, "job 2")
+
+	assert.Equal(t, 1, testOctopus.jobQueue.totalJobs)
+}
+
+// Test for checking the behavior when an octopus is given an invalid job.
+func TestOctopusInvalidJob(t *testing.T) {
+	testOctopus := NewOctopus(10)
+
+	err := testOctopus.HandleJob(nil)
+
+	assert.Equal(t, "invalid function", err.Error())
 }
